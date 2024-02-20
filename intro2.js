67d15920ef6d752191554a51a9f87e77d80f9d2f
OutlawDev <siyabonga.ncube669@gmail.com>
2024-02-20 15:03:10 +0200

    intro 2

---
+++ intro2.js
@@ -0,0 +1,13 @@
+var myVariable = "Hello, world!";
+
+var name = "John"; // String
+var age = 30; // Number
+var isStudent = true; // Boolean
+var fruits = ["apple", "banana", "orange"]; // Array
+var person = { firstName: "John", lastName: "Doe" }; // Object
+
+var myVariable = 10;
+console.log(myVariable); // Output: 10
+
+myVariable = "Hello";
+console.log(myVariable); // Output: Hello
